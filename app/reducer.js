13f2512c5c59e5893652a948f141b903a6a437bb
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-02-24 11:45:56 -0600

    finished reducer with tests

--- app/reducer.js
+++ app/reducer.js
@@ -1,3 +1,10 @@
-export default function reducer () {
-
+export default function reducer(state, action) {
+  switch (action.type) {
+    case 'CONTACT@FIND_ALL':
+      return { contacts: action.data };
+    case 'CONTACT@CREATE':
+      return { contacts: [...state.contacts, ...action.data] };
+    default:
+      return state || { contacts: [] };
+  }
 }

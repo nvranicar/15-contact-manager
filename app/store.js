b272ef7115dad2640b46c93f8d4c7fc332863c6f
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-02-24 09:48:35 -0600

    created store boilerplate

---
+++ app/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducer from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = { contacts: [] };
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+
+export default store;
